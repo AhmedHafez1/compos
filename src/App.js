ea4097ced230c2f689dfeba290d65e8aba7d301e
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-03 07:54:51 +0300

    Use PropTypes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ function App() {
   return (
     <div>
       <div>
-        <Button>Primary</Button>
+        <Button primary>Primary</Button>
       </div>
       <div>
-        <Button>Secondary</Button>
+        <Button secondary>Secondary</Button>
       </div>
       <div>
-        <Button>Success</Button>
+        <Button success>Success</Button>
       </div>
       <div>
-        <Button>Warning</Button>
+        <Button warning>Warning</Button>
       </div>
       <div>
-        <Button>Danger</Button>
+        <Button danger>Danger</Button>
       </div>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,38 @@
-const Button = ({ children }) => {
+import PropTypes from 'prop-types';
+
+const Button = ({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) => {
   return <button>{children}</button>;
+};
+
+Button.propTypes = {
+  primary: PropTypes.bool,
+  secondary: PropTypes.bool,
+  success: PropTypes.bool,
+  warning: PropTypes.bool,
+  danger: PropTypes.bool,
+  outline: PropTypes.bool,
+  rounded: PropTypes.bool,
+  onlyOneType: ({ primary, secondary, success, warning, danger }) => {
+    if (
+      Number(!!primary) +
+        Number(!!secondary) +
+        Number(!!success) +
+        Number(!!warning) +
+        Number(!!danger) >
+      1
+    ) {
+      return new Error('Use Only one type for buttons');
+    }
+  },
 };
 
 export default Button;
