e9245f21c4450744b4ea1c484886792135b62dbb
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-09 18:20:23 +0300

    conditional rendering

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+
 const Accordion = ({ items }) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
   const renderedItems = items.map(({ label, content }, i) => {
     return (
       <div key={i}>
         <h4>{label}</h4>
-        <p>{content}</p>
+        <p>{i === expandedIndex && content}</p>
       </div>
     );
   });
