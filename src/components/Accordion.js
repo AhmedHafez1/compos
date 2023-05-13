eb53c07124c483cd4c0becc2d86e1098254825d1
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-13 18:40:20 +0300

    Conditionally rendering icon

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,19 @@
 import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
 
 const Accordion = ({ items }) => {
   const [expandedIndex, setExpandedIndex] = useState(0);
 
   const renderedItems = items.map(({ label, content }, i) => {
+    const isExpanded = i === expandedIndex;
+    const icon = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    );
     return (
       <div key={i}>
         <h4 onClick={() => setExpandedIndex(i)}>{label}</h4>
-        <p>{i === expandedIndex && content}</p>
+        {icon}
+        <p>{isExpanded && content}</p>
       </div>
     );
   });
