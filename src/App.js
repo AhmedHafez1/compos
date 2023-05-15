b85ba47d2b4077123e3fe24afe3cd1160aed0208
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-05-15 15:03:24 +0300

    handle selection and selection state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import Dropdwon from './components/Dropdown';
+import Dropdwon from "./components/Dropdown";
+import { useState } from "react";
 
 function App() {
+  const [selectedColor, setSelectedColor] = useState(null);
+  const handleSlectedColor = (color) => {
+    setSelectedColor(color);
+  };
   const options = [
-    { label: 'Red', value: 'red' },
-    { label: 'Green', value: 'green' },
-    { label: 'Blue', value: 'blue' },
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
   ];
   return (
     <div>
-      <Dropdwon options={options}></Dropdwon>
+      <Dropdwon
+        options={options}
+        handleSelect={handleSlectedColor}
+        selectedOption={selectedColor}
+      ></Dropdwon>
     </div>
   );
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,21 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const Dropdwon = ({ options }) => {
+const Dropdwon = ({ options, selectedOption, handleSelect }) => {
   const [open, setOpen] = useState(false);
 
   const toggleOpen = () => setOpen(!open);
   const onSelect = (option) => {
     setOpen(false);
-    console.log(option.label);
+    handleSelect(option);
   };
 
   const renderedOptions = options.map((option) => (
-    <h5 onClick={() => onSelect(option)}>{option.label}</h5>
+    <h5 onClick={() => onSelect(option)} key={option.value}>
+      {option.label}
+    </h5>
   ));
+
+  let content = "Select...";
+  if (selectedOption) {
+    content = selectedOption.label;
+  }
 
   return (
     <div>
-      <div onClick={toggleOpen}>Select...</div>
+      <div onClick={toggleOpen}>{content}</div>
       {open && <div>{renderedOptions}</div>}
     </div>
   );
