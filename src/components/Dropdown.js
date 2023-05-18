8dee677f5bde131dbea530ec74ce7afaa1131d5a
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-05-18 10:05:38 +0300

    Added Panel reusable component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,16 +1,22 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from "./Panel";
 
 const Dropdwon = ({ options, value, onChange }) => {
-  const [open, setOpen] = useState(false);
+  const [isOpen, setOpen] = useState(false);
 
-  const toggleOpen = () => setOpen(!open);
+  const toggleOpen = () => setOpen(!isOpen);
   const onSelect = (option) => {
     setOpen(false);
     onChange(option);
   };
 
   const renderedOptions = options.map((option) => (
-    <h5 onClick={() => onSelect(option)} key={option.value}>
+    <h5
+      className="hover:bg-sky-100 rounded cursor-pointer p-1"
+      onClick={() => onSelect(option)}
+      key={option.value}
+    >
       {option.label}
     </h5>
   ));
@@ -18,9 +24,15 @@ const Dropdwon = ({ options, value, onChange }) => {
   let labelPlaceHolder = value?.label || "Select...";
 
   return (
-    <div>
-      <div onClick={toggleOpen}>{labelPlaceHolder}</div>
-      {open && <div>{renderedOptions}</div>}
+    <div className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
+        onClick={toggleOpen}
+      >
+        {labelPlaceHolder}
+        <GoChevronDown />
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
     </div>
   );
 };
