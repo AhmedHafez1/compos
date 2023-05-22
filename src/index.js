0a2af3e9d4639acb2c93dd4501d15a6a59fa9d6c
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-05-22 08:55:30 +0300

    NavigationContext

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { NavigationProvider } from "./context/navigation";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);
