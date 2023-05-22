3ab2e03fc1e2bb64b16616380c3175e0d72133cc
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-05-22 09:47:38 +0300

    setup pope state event listener

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,10 +1,21 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const NavigationContext = createContext();
 
 const NavigationProvider = ({ children }) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => setCurrentPath(window.location.pathname);
+    window.addEventListener("popstate", handler);
+    return () => {
+      window.removeEventListener("popstate", handler);
+    };
+  }, []);
+
   return (
     <NavigationContext.Provider value={{}}>
+      {currentPath}
       {children}
     </NavigationContext.Provider>
   );
