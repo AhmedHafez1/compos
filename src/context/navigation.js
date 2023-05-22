f3971548626e8e24e01fa7a991a68429f7e2de48
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-05-22 12:25:31 +0300

    shared value from nav context

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -13,9 +13,13 @@ const NavigationProvider = ({ children }) => {
     };
   }, []);
 
+  const navigate = (to) => {
+    window.history.pushState({}, "", to);
+    setCurrentPath(to);
+  };
+
   return (
-    <NavigationContext.Provider value={{}}>
-      {currentPath}
+    <NavigationContext.Provider value={{ currentPath, navigate }}>
       {children}
     </NavigationContext.Provider>
   );
